4515ac90df8cb196ee3b5d20ff71059305946246
David-Odhiambo <prof.odhiambodavid@gmail.com>
2022-09-07 13:36:49 +0300

    add

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-# script that configures an Ubuntu server with the below requirements
-# localhost resolves to 127.0.0.2
-# facebook.com resolves to 8.8.8.8
-
-cp /etc/hosts ~/hosts.new
-sed -i s/127.0.0.1/127.0.0.2/ ~/hosts.new
-echo -e "8.8.8.8\tfacebook.com" >>~/hosts.new
-cp -f ~/hosts.new /etc/hosts
+-- List all privileges for users 'user_0d_1' and 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
